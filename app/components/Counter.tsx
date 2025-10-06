6488eb7926f4b6eafb0e05f86a6577fb558dbcb4
Daniel Sadiq <danielsadiq93@gmail.com>
2025-10-06 12:47:57 +0100

    starter files update

---
+++ app/components/Counter.tsx
@@ -0,0 +1,13 @@
+"use client"
+import { useState } from "react"
+
+export default function Counter({users}) {
+  const [count, setCount] = useState(0);
+  console.log(users)
+  return (
+    <div className="flex gap-x-4 items-center">
+      {count}
+      <button className="px-6 py-2 rounded-md border-2 border-black" onClick={()=> setCount(c=> c+1)}>Increase</button>
+    </div>
+  )
+}
