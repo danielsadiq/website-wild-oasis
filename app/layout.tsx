bae2b0d296d480044799e665c941132cf7a9dc8a
Daniel Sadiq <danielsadiq93@gmail.com>
2025-10-06 16:16:35 +0100

    header and bg image add

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
-import Logo from "@/app/components/Logo";
-import Navigation from "@/app/components/Navigation";
 import "@/app/styles/globals.css";
 
 import {Josefin_Sans} from "next/font/google"
+import Header from "./components/Header";
 
 const josefin = Josefin_Sans({
   subsets: ['latin'],
@@ -26,13 +25,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${josefin.className} p-6 bg-primary-950 min-h-screen text-primary-100`}>
-        <div className="flex flex-col gap-6">
-          <Logo/>
-          <Navigation/>
-          <main>{children}</main>
+      <body className={`${josefin.className} bg-primary-950 min-h-screen text-primary-100 flex flex-col antialiased`}>
+          <Header/>
+          <div className="flex-1"><main className="max-w-7xl mx-auto px-8 py-12">{children}</main></div>
           <footer></footer>
-        </div>
       </body>
     </html>
   );
