9e63894c500ee23b5db91d148804fff7d23c5e51
Daniel Sadiq <danielsadiq93@gmail.com>
2025-11-01 03:21:49 +0100

    filter update

--- app/components/Filter.tsx
+++ app/components/Filter.tsx
@@ -1,24 +1,73 @@
-"use client"
+"use client";
 
-import { usePathname, useRouter, useSearchParams } from "next/navigation"
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { ReactNode } from "react";
 
 export default function Filter() {
   const searchParams = useSearchParams();
   const router = useRouter();
-  function handleFilter(filter:string){
+  const pathname = usePathname();
+  const activeFilter = searchParams.get("capacity") ?? "all";
+  function handleFilter(filter: string) {
     const params = new URLSearchParams(searchParams);
-    const pathname = usePathname();
     console.log(filter);
     params.set("capacity", filter);
-    router.replace(`${pathname}?${params.toString()}`, {scroll:false})
+    router.replace(`${pathname}?${params.toString()}`, { scroll: false });
   }
 
   return (
     <div className="border border-primary-800 flex">
-      <button className="px-5 py-2 hover:bg-pirmary-700 cursor-pointer" onClick={()=> handleFilter("all")}>All cabins</button>
-      <button className="px-5 py-2 hover:bg-pirmary-700 cursor-pointer" onClick={()=> handleFilter("small")}>1&mdash;3 cabins</button>
-      <button className="px-5 py-2 hover:bg-pirmary-700 cursor-pointer" onClick={()=> handleFilter("medium")}>4&mdash;7 cabins</button>
-      <button className="px-5 py-2 hover:bg-pirmary-700 cursor-pointer" onClick={()=> handleFilter("large")}>8&mdash;12 cabins</button>
+      <Button
+        filter="all"
+        handleFilter={handleFilter}
+        activeFilter={activeFilter}
+      >
+        All cabins
+      </Button>
+      <Button
+        filter="small"
+        handleFilter={handleFilter}
+        activeFilter={activeFilter}
+      >
+        1&mdash;3 cabins
+      </Button>
+      <Button
+        filter="medium"
+        handleFilter={handleFilter}
+        activeFilter={activeFilter}
+      >
+        4&mdash;7 cabins
+      </Button>
+      <Button
+        filter="large"
+        handleFilter={handleFilter}
+        activeFilter={activeFilter}
+      >
+        8&mdash;12 cabins
+      </Button>
     </div>
-  )
+  );
+}
+
+function Button({
+  children,
+  filter,
+  handleFilter,
+  activeFilter,
+}: {
+  children: ReactNode;
+  filter: string;
+  handleFilter: (val: string) => void;
+  activeFilter: string;
+}) {
+  return (
+    <button
+      className={`px-5 py-2 hover:bg-pirmary-700 cursor-pointer ${
+        filter === activeFilter ? "bg-primary-700 text-primary-50" : ""
+      }`}
+      onClick={() => handleFilter(filter)}
+    >
+      {children}
+    </button>
+  );
 }
